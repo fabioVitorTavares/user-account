b99df34cf5e22a869e9f8d3dbd873a2c86f9d058
fabioVitoTavares <fabio.furtado@estudante.ufla.br>
2023-11-24 23:45:59 -0300

    okk

--- src/Screens/Register/Register.tsx
+++ src/Screens/Register/Register.tsx
@@ -1,5 +1,28 @@
-import {} from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 export default function Register() {
-  return <h1>Register</h1>;
+  const [count] = useState<number>(0);
+
+  const c = useRef<number>(0);
+
+  useLayoutEffect(() => {
+    console.log("[9]: >>>", c);
+  }, [c]);
+
+  useEffect(() => {
+    console.log("[13]: >>>", c);
+  }, [c]);
+
+  function increment() {
+    console.log("[17]: >>>", c);
+    c.current = c.current + 1;
+  }
+
+  return (
+    <>
+      <h1> Register</h1>
+      <h1> {c.current}</h1>
+      <button onClick={increment}>count</button>
+    </>
+  );
 }
